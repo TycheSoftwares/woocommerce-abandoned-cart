12426579a0c4d2f4e20d3033a9b045e42cd2807d
moxajogani <moxa@tychesoftwares.com>
2015-05-12 17:54:09 +0530

    Version 1.4

--- woocommerce-abandoned-cart/woocommerce-ac.php
+++ woocommerce-abandoned-cart/woocommerce-ac.php
@@ -3,7 +3,7 @@
 Plugin Name: WooCommerce Abandon Cart Lite Plugin
 Plugin URI: http://www.tychesoftwares.com/store/premium-plugins/woocommerce-abandoned-cart-pro
 Description: This plugin captures abandoned carts by logged-in users & emails them about it. <strong><a href="http://www.tychesoftwares.com/store/premium-plugins/woocommerce-abandoned-cart-pro">Click here to get the PRO Version.</a></strong>
-Version: 1.3
+Version: 1.4
 Author: Ashok Rane
 Author URI: http://www.tychesoftwares.com/
 */
