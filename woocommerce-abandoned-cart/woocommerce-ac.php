7730187e5e0db7987dbd377f92b4eb1e88c9bcc7
bhavik-kiri <kiribhavik4@gmail.com>
2016-01-20 11:07:03 +0530

    Version 2.6
    The change log for this is as a below:
    
    There are 2 new features and 1 issue fix in this version which are
    mentioned as below:
    
    1) New setting named as “Send From This Email Address:” is added in Add
    / Edit template page. It will allow to change the From Email address of
    abandoned cart notifications.
    
    2) New setting named as “Send Reply Emails to:” is added in Add / Edit
    template page. It will allow to change the Reply to email address of the
    abandoned cart notifications.
    
    3) If the wp-content folder name is changed using iThemes Security
    (formerly Better WP Security) plugin then abandoned cart email
    notification were not sent. This has been fixed.

--- woocommerce-abandoned-cart/woocommerce-ac.php
+++ woocommerce-abandoned-cart/woocommerce-ac.php
@@ -3,7 +3,7 @@
 Plugin Name: WooCommerce Abandon Cart Lite Plugin
 Plugin URI: http://www.tychesoftwares.com/store/premium-plugins/woocommerce-abandoned-cart-pro
 Description: This plugin captures abandoned carts by logged-in users & emails them about it. <strong><a href="http://www.tychesoftwares.com/store/premium-plugins/woocommerce-abandoned-cart-pro">Click here to get the PRO Version.</a></strong>
-Version: 2.5.1
+Version: 2.6
 Author: Tyche Softwares
 Author URI: http://www.tychesoftwares.com/
 */
