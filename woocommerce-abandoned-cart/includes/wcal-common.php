e41c7f58d87564497733c656f59161fb4dd98490
Chetna patel <chetna@tychesoftwares.com>
2017-02-22 15:00:42 +0530

    Add new common file
    We have added new common file for all the views of users. So we can
    access the common functions if required.

---
+++ woocommerce-abandoned-cart/includes/wcal-common.php
@@ -0,0 +1,46 @@
+<?php
+
+class wcal_common {    
+    function wcal_get_abandoned_order_count( $get_section_result ){
+        global $wpdb;
+        $return_abandoned_count = 0;    
+        $blank_cart_info        = '{"cart":[]}';
+        $blank_cart_info_guest  = '[]';    
+        $ac_cutoff_time         = get_option( 'ac_lite_cart_abandoned_time' );
+        $cut_off_time           = $ac_cutoff_time * 60;
+        $current_time           = current_time( 'timestamp' );
+        $compare_time           = $current_time - $cut_off_time;
+    
+        switch ( $get_section_result ) {
+            case 'wcal_all_abandoned':    
+                $query_ac        = "SELECT * FROM `".$wpdb->prefix."ac_abandoned_cart_history_lite` WHERE ( user_type = 'REGISTERED' AND abandoned_cart_info NOT LIKE '%$blank_cart_info%' AND abandoned_cart_time <= '$compare_time' AND recovered_cart = 0 ) OR ( user_type = 'GUEST' AND abandoned_cart_info NOT LIKE '$blank_cart_info_guest' AND abandoned_cart_info NOT LIKE '%$blank_cart_info%' AND recovered_cart = 0  ) ORDER BY recovered_cart desc ";
+                $ac_results      = $wpdb->get_results( $query_ac );
+                $return_abandoned_count = count( $ac_results );
+                break;
+    
+            case 'wcal_all_registered':    
+                $query_ac        = "SELECT * FROM `".$wpdb->prefix."ac_abandoned_cart_history_lite` WHERE ( user_type = 'REGISTERED' AND abandoned_cart_info NOT LIKE '%$blank_cart_info%' AND abandoned_cart_time <= '$compare_time' AND recovered_cart = 0 ) ORDER BY recovered_cart desc ";
+                $ac_results      = $wpdb->get_results( $query_ac );
+                $return_abandoned_count = count( $ac_results );
+                break;
+    
+            case 'wcal_all_guest':
+                $query_ac        = "SELECT * FROM `".$wpdb->prefix."ac_abandoned_cart_history_lite` WHERE ( user_type = 'GUEST' AND abandoned_cart_info NOT LIKE '$blank_cart_info_guest' AND abandoned_cart_info NOT LIKE '%$blank_cart_info%' AND abandoned_cart_time <= '$compare_time' AND recovered_cart = 0 AND user_id >= 63000000 ) ORDER BY recovered_cart desc ";
+                $ac_results      = $wpdb->get_results( $query_ac );
+                $return_abandoned_count = count( $ac_results );    
+                break;
+    
+            case 'wcal_all_visitor':
+                $query_ac        = "SELECT * FROM `".$wpdb->prefix."ac_abandoned_cart_history_lite` WHERE ( user_type = 'GUEST' AND abandoned_cart_info NOT LIKE '$blank_cart_info_guest' AND abandoned_cart_info NOT LIKE '%$blank_cart_info%' AND abandoned_cart_time <= '$compare_time' AND recovered_cart = 0  AND user_id = 0 ) ORDER BY recovered_cart desc ";
+                $ac_results      = $wpdb->get_results( $query_ac );
+                $return_abandoned_count = count( $ac_results );    
+                break;
+    
+            default:
+                # code...
+                break;
+        }    
+        return $return_abandoned_count;
+    }
+}
+?>
--- woocommerce-abandoned-cart/woocommerce-ac.php
+++ woocommerce-abandoned-cart/woocommerce-ac.php
@@ -18,7 +18,7 @@ require_once( "includes/wcal_default-settings.php" );
 require_once( "includes/wcal_actions.php" );
 require_once( "includes/classes/class-wcal-aes.php" );
 require_once( "includes/classes/class-wcal-aes-counter.php" );
-
+require_once( "includes/wcal-common.php" );
 // Add a new interval of 15 minutes
 add_filter( 'cron_schedules', 'wcal_add_cron_schedule' );
 
@@ -1847,60 +1847,10 @@ if( !class_exists( 'woocommerce_abandon_cart_lite' ) ) {
     	          ?>	
                     	<p> <?php _e( 'The list below shows all Abandoned Carts which have remained in cart for a time higher than the "Cart abandoned cut-off time" setting.', 'woocommerce-ac' );?> </p>
                     	<?php
-                        
-                        
-                        function wcal_get_abandoned_order_count( $get_section_result ){
-                            global $wpdb;
-                            $return_abandoned_count = 0;
-                        
-                            $blank_cart_info         = '{"cart":[]}';
-                            $blank_cart_info_guest   = '[]';
-                        
-                            $ac_cutoff_time          = get_option( 'ac_lite_cart_abandoned_time' );
-                            $cut_off_time            = $ac_cutoff_time * 60;
-                            $current_time            = current_time( 'timestamp' );
-                            $compare_time            = $current_time - $cut_off_time;
-                        
-                            switch ( $get_section_result ) {
-                                case 'wcal_all_abandoned':
-                        
-                                    $query_ac        = "SELECT * FROM `".$wpdb->prefix."ac_abandoned_cart_history_lite` WHERE ( user_type = 'REGISTERED' AND abandoned_cart_info NOT LIKE '%$blank_cart_info%' AND abandoned_cart_time <= '$compare_time' AND recovered_cart = 0 ) OR ( user_type = 'GUEST' AND abandoned_cart_info NOT LIKE '$blank_cart_info_guest' AND abandoned_cart_info NOT LIKE '%$blank_cart_info%' AND recovered_cart = 0  ) ORDER BY recovered_cart desc ";
-                                    $ac_results      = $wpdb->get_results( $query_ac );
-                                    $return_abandoned_count = count( $ac_results );
-                                    break;
-                        
-                                case 'wcal_all_registered':
-                        
-                                    $query_ac        = "SELECT * FROM `".$wpdb->prefix."ac_abandoned_cart_history_lite` WHERE ( user_type = 'REGISTERED' AND abandoned_cart_info NOT LIKE '%$blank_cart_info%' AND abandoned_cart_time <= '$compare_time' AND recovered_cart = 0 ) ORDER BY recovered_cart desc ";
-                                    $ac_results      = $wpdb->get_results( $query_ac );
-                                    $return_abandoned_count = count( $ac_results );
-                                    break;
-                        
-                                case 'wcal_all_guest':
-                                    $query_ac        = "SELECT * FROM `".$wpdb->prefix."ac_abandoned_cart_history_lite` WHERE ( user_type = 'GUEST' AND abandoned_cart_info NOT LIKE '$blank_cart_info_guest' AND abandoned_cart_info NOT LIKE '%$blank_cart_info%' AND abandoned_cart_time <= '$compare_time' AND recovered_cart = 0 AND user_id >= 63000000 ) ORDER BY recovered_cart desc ";
-                                    $ac_results      = $wpdb->get_results( $query_ac );
-                                    $return_abandoned_count = count( $ac_results );
-                        
-                                    break;
-                        
-                                case 'wcal_all_visitor':
-                                    $query_ac        = "SELECT * FROM `".$wpdb->prefix."ac_abandoned_cart_history_lite` WHERE ( user_type = 'GUEST' AND abandoned_cart_info NOT LIKE '$blank_cart_info_guest' AND abandoned_cart_info NOT LIKE '%$blank_cart_info%' AND abandoned_cart_time <= '$compare_time' AND recovered_cart = 0  AND user_id = 0 ) ORDER BY recovered_cart desc ";
-                                    $ac_results      = $wpdb->get_results( $query_ac );
-                                    $return_abandoned_count = count( $ac_results );
-                        
-                                    break;
-                        
-                                default:
-                                    # code...
-                                    break;
-                            }
-                        
-                            return $return_abandoned_count;
-                        }
-                        $get_all_abandoned_count      = wcal_get_abandoned_order_count( 'wcal_all_abandoned' );
-                        $get_registered_user_ac_count = wcal_get_abandoned_order_count( 'wcal_all_registered' );
-                        $get_guest_user_ac_count      = wcal_get_abandoned_order_count( 'wcal_all_guest' );
-                        $get_visitor_user_ac_count    = wcal_get_abandoned_order_count( 'wcal_all_visitor' );
+                    	$get_all_abandoned_count      = wcal_common::wcal_get_abandoned_order_count( 'wcal_all_abandoned' );
+                    	$get_registered_user_ac_count = wcal_common::wcal_get_abandoned_order_count( 'wcal_all_registered' );
+                    	$get_guest_user_ac_count      = wcal_common::wcal_get_abandoned_order_count( 'wcal_all_guest' );
+                    	$get_visitor_user_ac_count    = wcal_common::wcal_get_abandoned_order_count( 'wcal_all_visitor' );                    	
                         
             	        $wcal_user_reg_text = 'User';
                         if ( $get_registered_user_ac_count > 1){
@@ -1962,8 +1912,6 @@ if( !class_exists( 'woocommerce_abandon_cart_lite' ) ) {
                                 | <a href="admin.php?page=woocommerce_ac_page&action=listcart&wcal_section=wcal_all_visitor" class="<?php echo $wcal_all_visitor; ?>"><?php _e( " Visitor $wcal_user_vis_text ", 'woocommerce-ac' ) ;?> <span class = "count" > <?php echo "( $get_visitor_user_ac_count )" ?> </span></a> 
                             </li>
                             <?php } ?>
-    
-    
                         </ul>
                         
                         <?php 
