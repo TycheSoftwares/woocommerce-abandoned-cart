73685340f4e962efe28b1ee6f0551b0a32d41d21
bhavik <bhavik@bhavik>
2017-06-30 13:00:51 +0530

    Admin notices for pro version
    We have added 4 notices at the admin side. It will display after 15 days, 22 days, 30 days after the first notice has been ignored
    
    If The AC pro is activated the it will not show any notices.
    
    In third notice that is 22 days, we are showing it for ORDD lite, so ordd pro is activated then we wil not display the same notice.

---
+++ woocommerce-abandoned-cart/assets/js/wcal_ts_dismiss_notice.js
@@ -0,0 +1,36 @@
+// Make notices dismissible
+jQuery(document).ready( function() {
+	jQuery( '.notice.is-dismissible' ).each( function() {
+		
+		//console.log (this.className);
+		var wcal_get_name = this.className;
+		if (wcal_get_name.indexOf('wcal-tracker') !== -1){
+			var $this = jQuery( this ),
+				$button = jQuery( '<button type="button" class="notice-dismiss"><span class="wcal screen-reader-text"></span></button>' ),
+				btnText = commonL10n.dismiss || '';
+
+			// Ensure plain text
+			$button.find( '.screen-reader-text' ).text( btnText );
+
+			$this.append( $button );
+			$button.on( 'click.notice-dismiss', function( event ) {
+				console.log ('here');
+				//alert('This');
+				event.preventDefault();
+				$this.fadeTo( 100 , 0, function() {
+					//alert();
+					jQuery(this).slideUp( 100, function() {
+						jQuery(this).remove();
+						var data = {
+							action: "wcal_admin_notices"
+						};
+						var admin_url = jQuery( "#admin_url" ).val();
+							jQuery.post( admin_url + "/admin-ajax.php", data, function( response ) {
+						});
+					});
+				});
+			});
+
+		}
+	});
+});
