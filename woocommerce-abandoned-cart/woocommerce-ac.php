dc848fc31896516ba80e82a76e53505fd9a5fc3d
Chetna1510 <chetna@tychesoftwares.com>
2017-09-14 17:45:18 +0530

    WooCommerce and PHP header tag.
    We have added WooCommerce compatibility & Requires PHP header tags to
    the plugin.

--- woocommerce-abandoned-cart/woocommerce-ac.php
+++ woocommerce-abandoned-cart/woocommerce-ac.php
@@ -1,13 +1,16 @@
 <?php 
 /*
-Plugin Name: Abandoned Cart Lite for WooCommerce
-Plugin URI: http://www.tychesoftwares.com/store/premium-plugins/woocommerce-abandoned-cart-pro
-Description: This plugin captures abandoned carts by logged-in users & emails them about it. <strong><a href="http://www.tychesoftwares.com/store/premium-plugins/woocommerce-abandoned-cart-pro">Click here to get the PRO Version.</a></strong>
-Version: 4.3
-Author: Tyche Softwares
-Author URI: http://www.tychesoftwares.com/
-Text Domain: woocommerce-ac
-Domain Path: /i18n/languages/
+* Plugin Name: Abandoned Cart Lite for WooCommerce
+* Plugin URI: http://www.tychesoftwares.com/store/premium-plugins/woocommerce-abandoned-cart-pro
+* Description: This plugin captures abandoned carts by logged-in users & emails them about it. <strong><a href="http://www.tychesoftwares.com/store/premium-plugins/woocommerce-abandoned-cart-pro">Click here to get the PRO Version.</a></strong>
+* Version: 4.3
+* Author: Tyche Softwares
+* Author URI: http://www.tychesoftwares.com/
+* Text Domain: woocommerce-ac
+* Domain Path: /i18n/languages/
+* Requires PHP: 5.6
+* WC requires at least: 3.0.0
+* WC tested up to: 3.2.0
 */
 
 // Deletion Settings
