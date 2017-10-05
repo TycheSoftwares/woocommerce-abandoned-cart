e07f0ec9d4ee39ffdf6ef3e22c169bd92822980c
bhavik <bhavik@bhavik>
2017-10-05 11:17:05 +0530

    Tempalate Activate button chnaged to Toggle
    Now the activate button of the template has been chnaged to toggle button

---
+++ woocommerce-abandoned-cart/assets/js/wcal_template_activate.js
@@ -0,0 +1,40 @@
+jQuery(function( $ ) {
+
+	$('.wcal-switch.wcal-toggle-template-status').click(function(){
+
+		var $switch, state, new_state;
+
+		$switch = $(this);
+
+		if ( $switch.is('.wcal-loading') )
+			return;
+
+		state = $switch.attr( 'wcal-template-switch' );
+		new_state = state === 'on' ? 'off' : 'on';
+
+		$switch.addClass('wcal-loading');
+		$switch.attr( 'wcal-template-switch', new_state );
+
+		$.post( ajaxurl, {
+			action          : 'wcal_toggle_template_status',
+			wcal_template_id: $switch.attr( 'wcal-template-id' ),
+			current_state   : new_state
+		}, function( wcal_template_response ) {
+			if ( wcal_template_response.indexOf('wcal-template-updated') > -1){
+				var wcal_template_response_array = wcal_template_response.split ( ':' );
+
+				var wcal_deactivate_ids = wcal_template_response_array[1];
+				var wcal_split_all_ids  = wcal_deactivate_ids.split ( ',' );
+
+				for (i = 0; i < wcal_split_all_ids.length; i++) { 
+					var selelcted_id = wcal_split_all_ids[i];
+				
+					var $list = document.querySelector('[wcal-template-id="'+ selelcted_id+'"]');
+					$($list).attr('wcal-template-switch','off');
+				}
+				
+			}
+			$switch.removeClass('wcal-loading');
+		});
+	});
+});
