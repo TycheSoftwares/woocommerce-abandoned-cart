97ed4504baef159a993c4eae4e6cdde8925cad95
Pinal <pinalj1612@gmail.com>
2019-03-14 23:23:43 +0530

    Missing ;
    Missing ; in the commit for fix #443

--- woocommerce-abandoned-cart/cron/wcal_send_email.php
+++ woocommerce-abandoned-cart/cron/wcal_send_email.php
@@ -318,7 +318,7 @@ if ( !class_exists( 'woocommerce_abandon_cart_cron' ) ) {
                                                     $prod_image         = $product->get_image();
                                                     $image_url          = wp_get_attachment_url( get_post_thumbnail_id( $product_id ) );
                                                     if ( strpos( $image_url, '/' ) === 0 ) {
-                                                        $image_url = get_option('siteurl') . $image_url
+                                                        $image_url = get_option('siteurl') . $image_url;
                                                     }
                                                     if ( isset( $v->variation_id ) && '' != $v->variation_id ) {
                                                         $variation_id               = $v->variation_id;
