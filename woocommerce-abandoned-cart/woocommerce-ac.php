39009cace86c6fdcadbfa693d0ae9fba6f5c2e42
Dhruvin Shah <shah.dhruvin90@gmail.com>
2019-11-25 15:02:34 +0530

    Version 5.6.1
    Version 5.6.1

--- woocommerce-abandoned-cart/woocommerce-ac.php
+++ woocommerce-abandoned-cart/woocommerce-ac.php
@@ -5,7 +5,7 @@
 * Description: This plugin captures abandoned carts by logged-in users & emails them about it.
 * <strong><a href="http://www.tychesoftwares.com/store/premium-plugins/woocommerce-abandoned-cart-pro">Click here to get the
 * PRO Version.</a></strong>
-* Version: 5.6
+* Version: 5.6.1
 * Author: Tyche Softwares
 * Author URI: http://www.tychesoftwares.com/
 * Text Domain: woocommerce-abandoned-cart
@@ -119,7 +119,7 @@ if ( ! class_exists( 'woocommerce_abandon_cart_lite' ) ) {
             }
 
 			if ( ! defined( 'WCAL_PLUGIN_VERSION' ) ) {
-				define( 'WCAL_PLUGIN_VERSION', '5.6' );
+				define( 'WCAL_PLUGIN_VERSION', '5.6.1' );
             }
             $this->one_hour     = 60 * 60;
             $this->three_hours  = 3 * $this->one_hour;
@@ -1040,7 +1040,8 @@ if ( ! class_exists( 'woocommerce_abandon_cart_lite' ) ) {
 
             $track_guest_user_cart_from_cart = "";
             if ( isset( $track_guest_cart_from_cart_page ) ) {
-                $track_guest_user_cart_from_cart = $track_guest_cart_from_cart_page;
+                //$track_guest_user_cart_from_cart = $track_guest_cart_from_cart_page;
+                $track_guest_user_cart_from_cart = 'off';
             }
 
             if ( isset( $cut_off_time ) ) {
