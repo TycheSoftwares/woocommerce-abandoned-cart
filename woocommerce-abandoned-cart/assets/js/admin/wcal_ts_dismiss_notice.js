2637d8fd986cf713acb8b2bb7dbd8c4de5997ea2
Pinal <pinalj1612@gmail.com>
2020-01-29 14:39:08 +0530

    Fix #590
    Fixed an issue where the notice is dismissed when the user clicks on the
    post link.

--- woocommerce-abandoned-cart/assets/js/admin/wcal_ts_dismiss_notice.js
+++ woocommerce-abandoned-cart/assets/js/admin/wcal_ts_dismiss_notice.js
@@ -2,7 +2,7 @@
  * Update the DB to mark the notice as dismissed.
  */
 jQuery(document).ready( function() {
-	jQuery( '#wcal_cron_notice' ).on( 'click', function() {
+	jQuery( '#wcal_cron_notice' ).on( 'click', '.notice-dismiss', function() {
 		var data = {
 			notice: 'wcal_scheduler_update_dismiss',
 			action: "wcal_dismiss_admin_notice"
