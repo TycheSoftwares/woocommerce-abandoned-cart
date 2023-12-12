f3ac98d0f7440b3527fbf324128679f4e8e89ab5
pinalj <pinalj1612@gmail.com>
2023-12-12 15:29:59 +0530

    Fix #932
    Fix #932 - Redirect will now happen only if the user has come via a reminder link.

--- woocommerce-ac.php
+++ woocommerce-ac.php
@@ -280,7 +280,7 @@ if ( ! class_exists( 'woocommerce_abandon_cart_lite' ) ) {
 			// 5.15.0
 			add_action( 'wp_login', array( &$this, 'wcal_populate_wc_session' ) );
 			if ( '' === get_option( 'wcal_auto_login_users', '' ) ) {
-				add_filter( 'woocommerce_login_redirect', array( &$this, 'ts_redirect_login' ) );
+				add_filter( 'woocommerce_login_redirect', array( &$this, 'ts_redirect_login' ), 10, 1 );
 			}
 		}
 
@@ -2040,13 +2040,20 @@ if ( ! class_exists( 'woocommerce_abandon_cart_lite' ) ) {
 				wcal_common::wcal_set_cart_session( 'wcal_user_id', $user_id );
 			}
 		}
+
 		/**
 		 * Redirect on cart page after user logged in.
 		 *
+		 * @param string $url - URL user is currently on.
+		 *
 		 * @since 5.16.0
 		 */
-		public static function ts_redirect_login() {
-			$url = wc_get_cart_url();
+		public static function ts_redirect_login( $url ) {
+
+			if ( get_transient( 'wcal_email_sent_id' ) ) {
+				$redirect_url = wc_get_cart_url();
+				return $redirect_url;
+			}
 			return $url;
 		}
 
